4b86db307f738f7927884e3051ea86e85984a359
Justin Le <justin@jle.im>
2019-02-03 16:04:53 -0800

    trying out new path based on context and substitution

--- src/Dhall/Typed.hs
+++ src/Dhall/Typed.hs
@@ -60,6 +60,7 @@ import qualified Data.List.NonEmpty                        as NE
 import qualified Data.Sequence                             as Seq
 import qualified Data.Text                                 as T
 import qualified Dhall                         as D hiding (Type)
+import qualified Dhall.Context                             as D
 import qualified Dhall.Core                                as D
 import qualified Dhall.Map                                 as M
 import qualified Dhall.TypeCheck                           as D
@@ -287,46 +288,193 @@ optBuild = DTerm (FA (FA ((tv1 :-> tv0) :-> tv0 :-> tv0) :-> TOptional tv0)) $
     _       -> Nothing
 
 fromDhall
-    :: DType a
+    :: D.Context DTerm
+    -> DType a
     -> D.Expr () D.X
     -> Maybe a
-fromDhall = \case
-    TV _ -> const Nothing
-    FA e -> fromPi e
-    a :-> b -> fromFunction a b
-    TType -> \case
-      D.Natural      -> SomeDType <$> Just TNatural
-      D.Integer      -> SomeDType <$> Just TInteger
-      D.Double       -> SomeDType <$> Just TDouble
-      D.Text         -> SomeDType <$> Just TText
-      D.App D.List t -> fromDhall TType t <&> \case
-        SomeDType q -> SomeDType (TList q)
-      D.App D.Optional t -> fromDhall TType t <&> \case
-        SomeDType q -> SomeDType (TOptional q)
-      _              -> Nothing
-    TBool -> \case
-      D.BoolLit b -> Just b
-      _           -> Nothing
-    TNatural -> \case
-      D.NaturalLit n -> Just n
-      _              -> Nothing
-    TInteger -> \case
-      D.IntegerLit n -> Just n
-      _              -> Nothing
-    TDouble  -> \case
-      D.DoubleLit n  -> Just n
-      _              -> Nothing
-    TText -> \case
-      D.TextLit (D.Chunks [] t) -> Just t
-      _              -> Nothing
-    TList t  -> \case
-      D.ListLit _ xs -> traverse (fromDhall t) xs
-      _              -> Nothing
-    TOptional t -> \case
-      D.OptionalLit _ x -> traverse (fromDhall t) x
-      D.Some x          -> Just <$> fromDhall t x
-      D.App D.None _    -> Just Nothing         -- is this necessary?
-      _                 -> Nothing
+fromDhall ctx a = \case
+    D.Const _ -> Nothing
+    D.Var (D.V v i) -> do
+      DTerm b x <- D.lookup v i ctx
+      Refl      <- a ~# b
+      pure x
+    D.Lam v t y -> do
+      b :-> c     <- Just a
+      SomeDType d <- fromDhall ctx TType t
+      Refl        <- b ~# d
+      yType       <- either (const Nothing) Just $
+                        D.typeWith (D.insert v t D.empty) y
+      SomeDType e <- fromDhall ctx TType yType
+      Refl        <- c ~# e
+      pure $ \x -> fromMaybe (errorWithoutStackTrace "fromDhall: typecheck failure") $
+        fromDhall (D.insert v (DTerm b x) ctx) c y
+--     -- | > Pi "_" A B                               ~        A  -> B
+--     --   > Pi x   A B                               ~  ∀(x : A) -> B
+--     | Pi  Text (Expr s a) (Expr s a)
+--     -- | > App f a                                  ~  f a
+--     | App (Expr s a) (Expr s a)
+--     -- | > Let [Binding x Nothing  r] e             ~  let x     = r in e
+--     --   > Let [Binding x (Just t) r] e             ~  let x : t = r in e
+--     | Let (NonEmpty (Binding s a)) (Expr s a)
+--     -- | > Annot x t                                ~  x : t
+--     | Annot (Expr s a) (Expr s a)
+    D.Bool      | TType <- a -> Just (SomeDType TBool)
+    D.BoolLit b | TBool <- a -> Just b
+    D.BoolAnd x y
+      | TBool <- a -> (&&) <$> fromDhall ctx TBool x <*> fromDhall ctx TBool y
+    D.BoolOr  x y
+      | TBool <- a -> (||) <$> fromDhall ctx TBool x <*> fromDhall ctx TBool y
+    D.BoolEQ  x y
+      | TBool <- a -> (==) <$> fromDhall ctx TBool x <*> fromDhall ctx TBool y
+    D.BoolNE  x y
+      | TBool <- a -> (/=) <$> fromDhall ctx TBool x <*> fromDhall ctx TBool y
+    D.BoolIf  b x y -> do
+      b'    <- fromDhall ctx TBool b
+      case b' of
+        True  -> fromDhall ctx a x
+        False -> fromDhall ctx a y
+    D.Natural      | TType    <- a -> Just (SomeDType TNatural)
+    D.NaturalLit n | TNatural <- a -> Just n
+--     -- | > NaturalFold                              ~  Natural/fold
+--     | NaturalFold
+--     -- | > NaturalBuild                             ~  Natural/build
+--     | NaturalBuild
+--     -- | > NaturalIsZero                            ~  Natural/isZero
+--     | NaturalIsZero
+--     -- | > NaturalEven                              ~  Natural/even
+--     | NaturalEven
+--     -- | > NaturalOdd                               ~  Natural/odd
+--     | NaturalOdd
+--     -- | > NaturalToInteger                         ~  Natural/toInteger
+--     | NaturalToInteger
+--     -- | > NaturalShow                              ~  Natural/show
+--     | NaturalShow
+--     -- | > NaturalPlus x y                          ~  x + y
+--     | NaturalPlus (Expr s a) (Expr s a)
+--     -- | > NaturalTimes x y                         ~  x * y
+--     | NaturalTimes (Expr s a) (Expr s a)
+--     -- | > Integer                                  ~  Integer
+--     | Integer
+--     -- | > IntegerLit n                             ~  ±n
+--     | IntegerLit Integer
+--     -- | > IntegerShow                              ~  Integer/show
+--     | IntegerShow
+--     -- | > IntegerToDouble                          ~  Integer/toDouble
+--     | IntegerToDouble
+--     -- | > Double                                   ~  Double
+--     | Double
+--     -- | > DoubleLit n                              ~  n
+--     | DoubleLit Double
+--     -- | > DoubleShow                               ~  Double/show
+--     | DoubleShow
+--     -- | > Text                                     ~  Text
+--     | Text
+--     -- | > TextLit (Chunks [(t1, e1), (t2, e2)] t3) ~  "t1${e1}t2${e2}t3"
+--     | TextLit (Chunks s a)
+--     -- | > TextAppend x y                           ~  x ++ y
+--     | TextAppend (Expr s a) (Expr s a)
+--     -- | > List                                     ~  List
+--     | List
+--     -- | > ListLit (Just t ) [x, y, z]              ~  [x, y, z] : List t
+--     --   > ListLit  Nothing  [x, y, z]              ~  [x, y, z]
+--     | ListLit (Maybe (Expr s a)) (Seq (Expr s a))
+--     -- | > ListAppend x y                           ~  x # y
+--     | ListAppend (Expr s a) (Expr s a)
+--     -- | > ListBuild                                ~  List/build
+--     | ListBuild
+--     -- | > ListFold                                 ~  List/fold
+--     | ListFold
+--     -- | > ListLength                               ~  List/length
+--     | ListLength
+--     -- | > ListHead                                 ~  List/head
+--     | ListHead
+--     -- | > ListLast                                 ~  List/last
+--     | ListLast
+--     -- | > ListIndexed                              ~  List/indexed
+--     | ListIndexed
+--     -- | > ListReverse                              ~  List/reverse
+--     | ListReverse
+--     -- | > Optional                                 ~  Optional
+--     | Optional
+--     -- | > OptionalLit t (Just e)                   ~  [e] : Optional t
+--     --   > OptionalLit t Nothing                    ~  []  : Optional t
+--     | OptionalLit (Expr s a) (Maybe (Expr s a))
+--     -- | > Some e                                   ~  Some e
+--     | Some (Expr s a)
+--     -- | > None                                     ~  None
+--     | None
+--     -- | > OptionalFold                             ~  Optional/fold
+--     | OptionalFold
+--     -- | > OptionalBuild                            ~  Optional/build
+--     | OptionalBuild
+--     -- | > Record       [(k1, t1), (k2, t2)]        ~  { k1 : t1, k2 : t1 }
+--     | Record    (Map Text (Expr s a))
+--     -- | > RecordLit    [(k1, v1), (k2, v2)]        ~  { k1 = v1, k2 = v2 }
+--     | RecordLit (Map Text (Expr s a))
+--     -- | > Union        [(k1, t1), (k2, t2)]        ~  < k1 : t1 | k2 : t2 >
+--     | Union     (Map Text (Expr s a))
+--     -- | > UnionLit k v [(k1, t1), (k2, t2)]        ~  < k = v | k1 : t1 | k2 : t2 >
+--     | UnionLit Text (Expr s a) (Map Text (Expr s a))
+--     -- | > Combine x y                              ~  x ∧ y
+--     | Combine (Expr s a) (Expr s a)
+--     -- | > CombineTypes x y                         ~  x ⩓ y
+--     | CombineTypes (Expr s a) (Expr s a)
+--     -- | > Prefer x y                               ~  x ⫽ y
+--     | Prefer (Expr s a) (Expr s a)
+--     -- | > Merge x y (Just t )                      ~  merge x y : t
+--     --   > Merge x y  Nothing                       ~  merge x y
+--     | Merge (Expr s a) (Expr s a) (Maybe (Expr s a))
+--     -- | > Constructors e                           ~  constructors e
+--     | Constructors (Expr s a)
+--     -- | > Field e x                                ~  e.x
+--     | Field (Expr s a) Text
+--     -- | > Project e xs                             ~  e.{ xs }
+--     | Project (Expr s a) (Set Text)
+--     -- | > Note s x                                 ~  e
+--     | Note s (Expr s a)
+--     -- | > ImportAlt                                ~  e1 ? e2
+--     | ImportAlt (Expr s a) (Expr s a)
+    _ -> Nothing
+
+
+
+-- fromDhall ctx = \case
+--     TV _ -> const Nothing
+--     FA e -> fromPi e
+--     a :-> b -> fromFunction a b
+--     TType -> \case
+--       D.Natural      -> SomeDType <$> Just TNatural
+--       D.Integer      -> SomeDType <$> Just TInteger
+--       D.Double       -> SomeDType <$> Just TDouble
+--       D.Text         -> SomeDType <$> Just TText
+--       D.App D.List t -> fromDhall ctx TType t <&> \case
+--         SomeDType q -> SomeDType (TList q)
+--       D.App D.Optional t -> fromDhall ctx TType t <&> \case
+--         SomeDType q -> SomeDType (TOptional q)
+--       _              -> Nothing
+--     TBool -> \case
+--       D.BoolLit b -> Just b
+--       _           -> Nothing
+--     TNatural -> \case
+--       D.NaturalLit n -> Just n
+--       _              -> Nothing
+--     TInteger -> \case
+--       D.IntegerLit n -> Just n
+--       _              -> Nothing
+--     TDouble  -> \case
+--       D.DoubleLit n  -> Just n
+--       _              -> Nothing
+--     TText -> \case
+--       D.TextLit (D.Chunks [] t) -> Just t
+--       _              -> Nothing
+--     TList t  -> \case
+--       D.ListLit _ xs -> traverse (fromDhall ctx t) xs
+--       _              -> Nothing
+--     TOptional t -> \case
+--       D.OptionalLit _ x -> traverse (fromDhall ctx t) x
+--       D.Some x          -> Just <$> fromDhall ctx t x
+--       D.App D.None _    -> Just Nothing         -- is this necessary?
+--       _                 -> Nothing
 
 fromPi :: DType e -> D.Expr () D.X -> Maybe (Forall e)
 fromPi a = \case
@@ -407,6 +555,16 @@ fromFunction a b = \case
     D.DoubleShow
       | (TDouble , TText   ) <- (a, b) -> Just (T.pack . show)
     _ -> Nothing
+
+-- substVal
+--     :: DType a
+--     -> DType b
+--     -> D.Var
+--     -> a
+--     -> D.Expr () D.X
+--     -> b
+-- substVal a b v x = \case
+
 
 toDhallType
     :: DType a
