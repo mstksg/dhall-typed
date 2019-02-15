4cc2e02b2512bbd52a8151fec44a50f2c59e9e17
Justin Le <justin@jle.im>
2019-02-14 16:42:53 -0800

    basis of the lambda calculus is here.  now just to find a way to ... generalize?

--- src/Dhall/Typed/LC.hs
+++ src/Dhall/Typed/LC.hs
@@ -32,6 +32,7 @@ data DKind :: [DSort] -> DSort -> Type where
     KVar :: Index ts a -> DKind ts a
     KLam :: SDSort t -> DKind (t ': ts) a -> DKind ts (t ':*> a)
     KP   :: KPrim as a -> Prod (DKind ts) as -> DKind ts a
+    KPi  :: SDSort t -> DKind (t ': ts) a -> DKind ts a
 
 data SKPrim as a :: KPrim as a -> Type where
     SKApp :: SKPrim '[ a ':*> b, a ] b 'KApp
@@ -49,20 +50,35 @@ type KType   = 'KP 'Type 'Ø
 type family SDKindOf ts k (x :: DKind ts k) = (y :: SDKind ts k x) | y -> x where
     SDKindOf ts k          ('KVar i  ) = 'SKVar (SIndexOf ts k i)
 
+type family KShift ts t a (x :: DKind ts a) :: DKind (t ': ts) a where
+
+type family MapKShift ts t a (xs :: [DKind ts a]) :: [DKind (t ': ts) a] where
+
+type family KSub ts rs a b (del :: Delete ts rs a) (x :: DKind rs a) (r :: DKind ts b) :: DKind rs b where
+
 data TPrim ts :: [DKind ts 'Kind] -> DKind ts 'Kind -> Type where
     TApp :: TPrim ts '[a :~> b, a] b
     TFun :: TPrim ts '[KType, KType] KType
     Bool :: TPrim ts '[] KType
+    List :: TPrim ts '[ KType ] KType
 
 data DType ts :: [DKind ts 'Kind] -> DKind ts 'Kind -> Type where
     TVar  :: Index us a -> DType ts us a
     TLam  :: SDKind ts 'Kind u -> DType ts (u ': us) a -> DType ts us (u :~> a)
+    Pi    :: SDKind ts 'Kind u -> DType ts (u ': us) a -> DType ts us a
+    TPoly :: SDSort t
+          -> DType (t ': ts) (MapKShift ts t 'Kind us) a
+          -> DType ts        us             ('KPi (SDSortOf t) a)
+    TInst :: DType ts us ('KPi (SDSortOf t) b)
+          -> SDKind ts t a
+          -> DType ts us (KSub (t ': ts) ts t 'Kind 'DZ a b)
     TP    :: TPrim ts as a -> Prod (DType ts us) as -> DType ts us a
 
 data STPrim ts as a :: TPrim ts as a -> Type where
     STApp :: STPrim ts '[ a :~> b, a ] b 'TApp
     STFun :: STPrim ts '[ KType, KType ] KType 'TFun
     SBool :: STPrim ts '[] KType 'Bool
+    SList :: STPrim ts '[KType] KType 'List
 
 data SDType ts us a :: DType ts us a -> Type where
     STVar  :: SIndex us a i -> SDType ts us a ('TVar i)
@@ -75,11 +91,24 @@ type a :-> b = 'TP 'TFun (a ':< b ':< 'Ø)
 type TBool   = 'TP 'Bool 'Ø
 
 data Prim ts us :: [DType ts us KType] -> DType ts us KType -> Type where
+    App     :: Prim ts us '[ a :-> b, a ] b
     BoolLit :: Bool -> Prim ts us '[] TBool
     BoolAnd :: Prim ts us '[TBool, TBool] TBool
     BoolNot :: Prim ts us '[] (TBool :-> TBool)
 
+type family Shift ts us u a (x :: DType ts us a) :: DType ts (u ': us) a where
+
+type family MapShift ts us u a (xs :: [DType ts us a]) :: [DType ts (u ': us) a] where
+
+type family Sub ts us qs a b (del :: Delete us qs a) (x :: DType ts qs a) (r :: DType ts us b) :: DType ts qs b where
+
 data DTerm ts (us :: [DKind ts 'Kind]) :: [DType ts us KType] -> DType ts us KType -> Type where
     Var  :: Index vs a -> DTerm ts us vs a
     Lam  :: SDType ts us KType v -> DTerm ts us (v ': vs) a -> DTerm ts us vs (v :-> a)
+    Poly :: SDKind ts 'Kind u
+         -> DTerm ts (u ': us) (MapShift ts us u KType vs) a
+         -> DTerm ts us vs ('Pi (SDKindOf ts 'Kind u) a)
+    Inst :: DTerm ts us vs ('Pi (SDKindOf ts 'Kind u) b)
+         -> SDType ts us u a
+         -> DTerm ts us vs (Sub ts (u ': us) us u KType 'DZ a b)
     P    :: Prim ts us as a -> Prod (DTerm ts us vs) as -> DTerm ts us vs a
